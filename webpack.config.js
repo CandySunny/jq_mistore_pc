9875a4f1a01722874a4c5fe9abf4d00598252585
DESKTOP-GA7JPEP\pingyi.jiang <meining.sun@biggetech.com>
2019-10-25 09:26:13 +0800

    首次入库，完成webpack配置

---
+++ webpack.config.js
@@ -0,0 +1,93 @@
+const path = require('path');
+const uglify = require('uglifyjs-webpack-plugin');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+const miniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const config = {
+    mode: 'development',//production
+    entry: {
+        index: path.resolve(__dirname, './src/js/index.js')
+    },
+    output: {
+        path: path.resolve(__dirname + '/dist'),
+        filename: 'js/[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: path.resolve(__dirname, 'node_modules'),
+                query: {
+                    'presets': ['latest']
+                }
+            },
+            {
+                test:/\.tpl$/,
+                loader: 'ejs-loader'
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    // {
+                    //     loader: miniCssExtractPlugin.loader,
+                    //     options: {
+                    //         hmr: process.env.NODE_ENV === 'development'
+                    //     }
+                    // },
+                    'style-loader',//负责把css放到html当中
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: function() {
+                                return [autoprefixer('last 5 versions')]
+                            }
+                        }
+                    },
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|ico)$/i,
+                loader: [
+                    'url-loader?limit=1024&name=img/[name]-[hash:16].[ext]',
+                    'image-webpack-loader'
+                ]
+            }
+        ]
+    },
+
+    plugins: [
+        new uglify(),
+        new htmlWebpackPlugin({
+            minify: {
+                removeComments: true,//删除所有注释
+                collapseWhitespace: true//删除所有空格换行
+            },
+            filename: 'index.html',//打包后的文件名称
+            template: path.resolve(__dirname, 'src/index.html'),//模板文件
+            title: 'mistore',
+            chunksSortMode: 'manual',
+            chunks: ['index'],
+            excludeChunks: ['node-modules'],
+            hash: true
+        })       
+
+        // new miniCssExtractPlugin({
+        //     filename: 'css/[name].css'
+        // })
+    ],
+
+    devServer: {
+        watchOptions: {
+            ignored: /node_modules/
+        },
+        open: true,
+        host: 'localhost',
+        port: 3200
+    }  
+}
+
+module.exports = config;
