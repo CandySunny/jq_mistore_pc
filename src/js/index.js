f75215d076a82c25078bd82c52e642b54feb59ec
DESKTOP-GA7JPEP\pingyi.jiang <meining.sun@biggetech.com>
2019-10-25 16:21:15 +0800

    完成header和logo组件

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,22 @@
-console.log(1)
+import '../scss/common.scss';
+import { Header } from '../components/header';
+
+class Index {
+    constructor() {
+        this.$app = $('<div id="app">');
+
+        this.init();
+    }
+
+    init() {
+        this.render();
+    }
+
+    render() {
+        new Header(this.$app).init();
+
+        $('body').prepend(this.$app);
+    }
+}
+
+new Index(jQuery);
